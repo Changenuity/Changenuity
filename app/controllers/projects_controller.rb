0b9dd2688752ae15c690a69d3276fe05ce37217e
eyqs <eugene@eyqs.ca>
2017-01-13 09:25:39 -0800

    Handle Post page with invalid submissions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,20 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(
+				:name, :date, :location, :description, :parameters, :references)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/post',    to: 'projects#new'
+  post '/post',    to: 'projects#create'
   resources :projects
 end
