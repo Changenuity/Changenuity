203068457e3eda2963a1a7ced937b7dad7d169aa
eyqs <eugene@eyqs.ca>
2017-01-16 08:28:53 +0100

    Fix post typo, update .gitignore for Paperclip

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ApplicationRecord
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png'], :message => 'file type is not allowed (only jpeg/png images)'
-
+	has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+	validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png'],
+                                            message: 'file type is not allowed (only jpeg/png images)'
   validates :name,  presence: true, length: { maximum: 255 }
   validates :date,  presence: true, length: { maximum: 255 }
   validates :location,              length: { maximum: 255 }
