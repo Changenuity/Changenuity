4c2d4f24dec774b9e93df7bf2dd59fad0e2b595e
eyqs <eugene@eyqs.ca>
2017-01-24 08:41:45 +0100

    Add Category and Type models and migcate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114215613) do
+ActiveRecord::Schema.define(version: 20170124074046) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +32,13 @@ ActiveRecord::Schema.define(version: 20170114215613) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
