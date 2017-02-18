8d732673acb03a00b36705591e5491453b514577
eyqs <eugene@eyqs.ca>
2017-02-18 17:52:11 +0100

    Make working login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Thank you for signing up for Changenuity!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  root 'static_pages#weebly'
-  get  '/home',    to: 'static_pages#home'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/post',    to: 'projects#new'
-  post '/post',    to: 'projects#create'
-  get  '/browse',  to: 'projects#index'
-  get  '/signup',  to: 'users#new'
+  root   'static_pages#weebly'
+  get    '/home',    to: 'static_pages#home'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/post',    to: 'projects#new'
+  post   '/post',    to: 'projects#create'
+  get    '/browse',  to: 'projects#index'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :projects
   resources :users
 end
