247dc540ec348999c4ae380510d494cb8f8f83b8
eyqs <eugene@eyqs.ca>
2017-03-04 21:21:59 +0100

    Reset columns

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,4 @@
 class Project < ApplicationRecord
-  belongs_to :category
-  belongs_to :type
 
   has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
   validates_attachment_content_type :image,
