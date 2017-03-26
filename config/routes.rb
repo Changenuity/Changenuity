ac4776fa1451437198ceb3ef8b278d9372ba76bb
eyqs <eugene@eyqs.ca>
2017-03-26 01:58:04 +0100

    Add terms and conditions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get   '/home',    to: 'static_pages#home'
   get   '/about',   to: 'static_pages#about'
   get   '/contact', to: 'static_pages#contact'
+  get   '/toc',     to: 'static_pages#toc'
   get   '/post',    to: 'projects#new'
   post  '/post',    to: 'projects#create'
   get   '/browse',  to: 'projects#index'
