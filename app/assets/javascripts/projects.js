fa1fc62d27359ee8e23b3ee4d475c8c43a26ef06
Xiao He Chen <xh3chen@uwaterloo.ca>
2017-07-01 15:01:14 -0400

    Homepage sign in first version complete

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -5,42 +5,36 @@
 // Sets up all bindings related to the Projects views
 
 $(function() {
-  // CONTENT
 
-  // STYLING
+  var clickOnFavButton = function() {
+    if( $(this).hasClass('active') ) {
+      $(this).siblings('.fav-count').html(function(){
+        return parseInt($(this).html()) - 1;
+      });
+    } else {
+      $(this).siblings('.fav-count').html(function(){
+        return parseInt($(this).html()) + 1;
+      });
+    }
+    $(this).toggleClass('active');
+  }
 
 
-  // Disabling event propagation when certain elements are clicked
-  $.each($('a, .fav-count, .btn-fav'), function() {
-    this.addEventListener('click', function(event) {
-      event.stopPropagation();
-    })
-  });
-
-
-  // Pseudo-effect of increasing number of favourites
-  $.each($('.btn-fav'), function() {
-    $(this).on('click', function(event) {
-      if( $(this).hasClass('active') ) {
-        $(this).siblings('.fav-count').html(function(){
-          return parseInt($(this).html()) - 1;
-        });
-      } else {
-        $(this).siblings('.fav-count').html(function(){
-          return parseInt($(this).html()) + 1;
-        });
-      }
-      $(this).toggleClass('active');
-    })
-  });
+  var dispatch = function(element) {
+    if( $(element).hasClass('btn-fav') ) {
+      clickOnFavButton.call(element);
+    }
+  }
 
 
   // BINDINGS
 
+
   // Adds bindings for when user clicks on a pcard (aka Project cards)
-  $.each($('.pcard'), function() {
-    this.addEventListener('click', function(event) {
-      window.location.href = $(this).attr('data-project-url');
-    }, false);
+  $('.pcard').on('click', function(){
+    window.location.href = $(this).attr('data-project-url');
+  }).find('a, button, .btn, .btn-fav').on('click', function(e){
+    e.stopPropagation();
+    dispatch(e.target);
   });
 });
