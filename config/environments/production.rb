6ce9e247769c6fd976d11d8b656257867461da34
eyqs <eugene@eyqs.ca>
2017-07-05 20:25:04 -0700

    Rollback destroy user path to use DELETE

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { host: 'changenuity-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'changenuity.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address: ENV.fetch('MAILER_ADDRESS'),
     port: 587,
