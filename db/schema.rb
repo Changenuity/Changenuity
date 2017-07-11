c554199020773e7daa3c61cde034f1e01aeba47b
Alison <alison.wu.0606@gmail.com>
2017-07-10 22:30:39 -0700

    Edit migration hm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706064157) do
+ActiveRecord::Schema.define(version: 20170711035325) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id"
