f3602ae43312da2247f48df807f831f1d3ceae0c
eyqs <eugene@eyqs.ca>
2017-07-14 20:04:47 -0700

    Add static pages controller tests, remove landing

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
@@ -60,13 +61,13 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-# group :test do
-#   gem 'rails-controller-testing'
-#   gem 'minitest-reporters'
-#   gem 'guard'
-#   gem 'guard-minitest'
-# end
-#
+group :test do
+  gem 'rails-controller-testing'
+  gem 'minitest-reporters'
+  gem 'guard'
+  gem 'guard-minitest'
+end
+
 group :production do
   gem 'pg'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,44 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+
+  def setup
+    @base_title = "Changenuity"
+  end
+
+  test 'should get root' do
+    get root_path
+    assert_response :success
+    assert_select 'title', @base_title
+  end
+
+  test 'should get home' do
     get home_path
     assert_response :success
+    assert_select 'title', @base_title
   end
 
-  test "should get about" do
+  test 'should get about' do
     get about_path
     assert_response :success
+    assert_select 'title', "About - #{@base_title}"
   end
 
-  test "should get contact" do
+  test 'should get contact' do
     get contact_path
     assert_response :success
+    assert_select 'title', "Contact - #{@base_title}"
   end
 
+  test 'should get privacy' do
+    get privacy_path
+    assert_response :success
+    assert_select 'title', "Privacy Policy - #{@base_title}"
+  end
+
+  test 'should get tos' do
+    get tos_path
+    assert_response :success
+    assert_select 'title', "Terms of Service - #{@base_title}"
+  end
 end
