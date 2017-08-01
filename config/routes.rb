65efbc763bc55d8c1357fcc120db0f07d0eb6797
eyqs <eugene@eyqs.ca>
2017-08-01 11:18:55 -0700

    Stop users from seeing undesigned Devise pages

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
     resources  :proposals
   end
   resources  :tags, only: [:index, :show]
-  devise_for :users, path: '', controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}, path_names: {sign_up: 'top_secret_sign_up'}
+  devise_for :users, path: '',
+    controllers: {omniauth_callbacks: 'users/omniauth_callbacks'},
+    path_names: {sign_in: 'top_secret_sign_in', sign_up: 'top_secret_sign_up'}
   get '/users/:username', to: 'users#show'
   resources :users, :only => [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20170720030019) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
