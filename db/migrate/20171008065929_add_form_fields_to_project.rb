088605829b65c348e92501d5149a92ecbf805cec
Eugene Y. Q. Shen <eugene@eyqs.ca>
2017-11-19 10:34:17 -0800

    Fix PostgreSQL malformed array literal error

--- db/migrate/20171008065929_add_form_fields_to_project.rb
+++ db/migrate/20171008065929_add_form_fields_to_project.rb
@@ -1,5 +1,5 @@
 class AddFormFieldsToProject < ActiveRecord::Migration[5.0]
   def change
-    add_column :projects, :form_fields, :string, array: true, default: [].to_yaml
+    add_column :projects, :form_fields, :string, array: true, default: []
   end
 end
