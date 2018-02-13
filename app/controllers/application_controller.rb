8387c64023c0422d26c0c3b544ac1618c20bcad4
Eugene Y. Q. Shen <eugene@eyqs.ca>
2018-02-12 20:25:45 -0800

    Add commitment field to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username,
-      :location, :biography, :experience, :organization, :passions, :skills, :work, :image])
+      :location, :biography, :experience, :organization, :passions, :skills, :work, :commitment, :image])
   end
 end
